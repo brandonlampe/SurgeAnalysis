86ddbd0d1c0e01365eee8f667d1b9b89c4dfa799
Brandon Lampe <brandon.lampe@gmail.com>
2016-01-25 12:21:56 -0700

    Completed transient surge analysis: MethodOfChar01.py
    - features of code:
        - changing upstream BC (influx of prod)
        - fluid properties calculated via CoolProp
        - auxillary functions stored in: SurgeFunc.py
        - automated figures of fluid pressure and velocity
    
    - next task:
        - need to write Sandia's functions for brine properites into SurgeFunc.py

--- MethodOfChar01.py
+++ MethodOfChar01.py
@@ -3,10 +3,12 @@
 -upstream (EOT) is always assumed to be the problem datum (elev=0)
 
 """
-
 import numpy as np
 import matplotlib.pyplot as plt
-# import sys
+import sys
+sys.path.append('/Users/Lampe/PyScripts/surgeAnalysis')
+import SurgeFunc as sf
+
 # import pdb
 
 # RUNS ON PYTHON VERSION 2.7+
@@ -14,44 +16,95 @@ import matplotlib.pyplot as plt
 np.set_printoptions(precision=4)
 
 
-def valprint(string, value):
-    """ Ensure uniform formatting of scalar value outputs. """
-    print("{0:>30}: {1: .6e}".format(string, value))
-
-
-def matprint(string, value):
-    """ Ensure uniform formatting of matrix value outputs. """
-    print("{0}:".format(string))
-    print(value)
-
-# Problem Data
-TIME_CLOSE = 5  # SECONDS (VALVE CLOSURE DURATION)
-TIME = 90  # SECONDS OF ANALYSIS
-LENGTH = 3.e3  # FEET (PIPE LENGTH)
-DIA = 2  # FEET (DIAMETER)
-WAVE_SPEED = 3.7E3  # FEET/SEC (SPEED OF SOUND IN FLUID)
+# INPUT DATA ###
+TIME_CLOSE = 4  # SECONDS (VALVE CLOSURE DURATION)
+TIME = 30  # SECONDS OF ANALYSIS
 VEL0 = 15.0  # FEET/SEC (INITIAL FLUID VELOCITY)
-HEAD_US = 4000.0  # FEET (ELEV + PRESSURE HEADS = TOTAL HEAD AT UPSTREAM DATUM)
+PRESS_US = 2000  # PSI (PRESSURE AT EOT)
 THETA = np.pi/2  # ORIENTATION OF PIPE (RADIANS: 0=HORIZ, np.pi/2=VERT)
-FRIC = 0.1  # DIMENSIONLESS DARCY-WIESBACH FRICTION FACTOR
 
-# GRID SPACING
+LENGTH = 3.e3  # FEET (PIPE LENGTH)
+DIA_IN = 24.0  # INCHES (INNER DIAMETER OF PIPE)
+ROUGH = 0.06  # INCHES (ROUGHNESS OF LEACHING STRING (0.018 TO 0.06))
+WALL_THICK = 0.75  # INCH (PIPE WALL THICKNESS)
+MOD_PIPE = 29E6  # PSI (MODULUS OF ELASTICITY FOR PIPE)
+POISSON = 0.27  # POISSION'S RATIO FOR STEEL
+
+# CONSTANTS
+GRAV = 32.2  # FT/SEC2
+
+# CONDITIONS FOR FLUID PROPERTIES
+FLUID = "Water"  # "n-Propane"  #
+FLUID_TEMP = 105.0  # DEG F
+FLUID_PRES = PRESS_US  # PSIG
+FLUID_WEIGHT = sf.unit_weight(FLUID_TEMP, FLUID_PRES, FLUID)  # LB/FT3
+FLUID_RHO = FLUID_WEIGHT / GRAV  # SLUG/FT3
+
+# PRODUCT PROPERTIES
+PROD = "n-Propane"
+PROD_TEMP = 105.0
+PROD_PRES = PRESS_US  # PSIG
+PROD_WEIGHT = sf.unit_weight(PROD_TEMP, PROD_PRES, PROD)  # LB/FT3
+PROD_RHO = PROD_WEIGHT / GRAV  # SLUG/FT3
+
+# DEFINE DISCRETIZATION
 NELEM = 4
+
+# CALCULATE WAVE SPEED IN SYSTEM
+SPG = FLUID_WEIGHT/62.4  # SPECIFIC GRAVITY OF FLUID
+sf.valprint("FLUID GRAVITY", SPG)
+FLUID_COMP = sf.compressibility(FLUID_TEMP, FLUID_PRES, FLUID)  # 1/PSI
+sf.valprint("FLUID COMPRESSIBILITY", FLUID_COMP)
+WAVE_SPEED_FLUID = sf.speedOfSound(FLUID_TEMP, FLUID_PRES, FLUID)  # FT/SEC
+sf.valprint("SPEED OF SOUND IN FLUID ONLY", WAVE_SPEED_FLUID)
+
+NUM = 144.0/(FLUID_COMP * FLUID_RHO)  # FT2/SEC2
+DEN = 1 + (DIA_IN/(MOD_PIPE * WALL_THICK * FLUID_COMP))*(1-POISSON/2.0)
+WAVE_SPEED = np.sqrt(NUM/DEN)
+sf.valprint("SPEED OF SOUND IN SYSTEM", WAVE_SPEED)
+TIME_CRIT = LENGTH/WAVE_SPEED  # TIME FOR PRESSURE WAVE TO TRAVEL LENGTH
+sf.valprint("CRIT TIME", TIME_CRIT)
+
+CHECK = DIA_IN/(MOD_PIPE * WALL_THICK) * (1 - POISSON/2) * (1/FLUID_COMP)
+sf.valprint("PIPE CHECK", CHECK)
+
+# UPSTREAM BOUNDARY CONDITION
+# FEET (ELEV + PRESSURE + VELOCITY HEADS = TOTAL HEAD AT UPSTREAM DATUM)
+HEAD_US = 0.0 + PRESS_US/FLUID_WEIGHT*144.0 + VEL0**2/(2*GRAV)
+sf.valprint("UPSTREAM BC (FT)", HEAD_US)
+
+# END INPUT DATA ###
 NNODE = NELEM + 1
 DELX = LENGTH/NELEM  # FEET (NODE SPACING)
 DELT = DELX/WAVE_SPEED  # SECOND (TIME STEP)
 XGRID = np.linspace(start=0, stop=LENGTH, num=NNODE)  # SPATIAL DISC
 TGRID = np.linspace(start=0, stop=TIME, num=TIME/DELT+1)  # TEMPORAL DISC
+COURANT = WAVE_SPEED * DELT/DELX  # THE COURANT NUMBER <= 0.5 FOR STABILITY
+sf.valprint("COURANT", COURANT)
+sf.valprint("Delta Time", DELT)
+sf.valprint("Delta X", DELX)
+CHECK = WAVE_SPEED * DELT
+sf.valprint("a * dt", CHECK)
 
-# CONSTANTS
-GRAV = 32.2  # FT/SEC2
-RHO = 1.94  # SLUGS/FT3
+
+# CALCULATIONS TO SET UP PROBLEM
+DIA = DIA_IN/12.0  # FEET (DIAMETER)
+AREA = np.pi * DIA**2/4.0  # FT2 (AREA OF PIPE FLOW)
+RHOW = 1.94  # SLUGS/FT3 (DENSITY OF WATER)
+RHO = FLUID_RHO  # SLUGS/FT3 (DENSITY OF FLUID)
 GAMMA = RHO * GRAV  # LB/FT3
+DEL_WEIGHT = (FLUID_WEIGHT - PROD_WEIGHT)  # LB/FT3 (DIFF IN FLUID WEIGHTS)
 
-# INITIAL CONDITIONS
+# CALCULATE FRICTION FACTOR
+VISC = sf.visc(90, 2000, "Water")
+# DIMENSIONLESS DARCY-WIESBACH FRICTION FACTOR
+FRIC = sf.FrictionFact_jit(VEL0, VISC, SPG, DIA_IN, ROUGH, 0)
+sf.valprint("FRICTION FACTOR", FRIC)
+
+# INITIAL CONDITIONS (TOTAL HEAD EVERYWHERE IN SYSTEM)
 HEAD_EL = np.sin(THETA)*XGRID  # ELEVATION HEAD
 HEAD_V = VEL0**2/(2*GRAV)  # VELOCITY HEAD
-HEAD_LS = FRIC*XGRID/(2*DIA*GRAV)*VEL0**2 # HEAD LOSS FROM FRICTION
+HEAD_LS = FRIC*XGRID/(2*DIA*GRAV)*VEL0**2  # HEAD LOSS FROM FRICTION
 HEAD0 = HEAD_US - HEAD_LS  # INITIAL CONDITIONS - TOTAL HEAD
 DEPTH = HEAD_EL[::-1]
 
@@ -67,6 +120,9 @@ for i in xrange(len(VEL_DS)):
 # DEFINE ARRAYS TO STORE THE SOLUTION
 HEAD_ARR = np.zeros((len(XGRID), len(TGRID)))
 VEL_ARR = np.zeros((len(XGRID), len(TGRID)))
+FRIC_ARR = np.zeros((len(XGRID), len(TGRID)))
+INT = np.zeros(len(TGRID))  # TRACKS INTERFACE DEPTH
+int_pass = np.zeros(len(TGRID)) 
 
 # SET INITIAL CONDITIONS
 HEAD_ARR[:, 0] = HEAD0
@@ -75,6 +131,7 @@ VEL_ARR[:, 0] = VEL0
 # SET BCTS
 HEAD_ARR[0, :] = HEAD_US  # UPSTREAM HEAD
 VEL_ARR[-1, :] = VEL_DS  # DOWNSTREAM VELOCITY
+FRIC_ARR[:, 0] = FRIC
 
 BTERM = WAVE_SPEED/GRAV
 RTERM = FRIC*DELX/(2*GRAV*DIA)
@@ -90,21 +147,33 @@ bpos = BTERM + RTERM*np.abs(VEL_ARR[-2, 0])
 cpos = HEAD_ARR[-2, 0] + BTERM*VEL_ARR[-2, 0]
 HEAD_ARR[-1, 1] = cpos - bpos*VEL_ARR[-1, 1]
 
+sf.valprint("DEL_WEIGHT", DEL_WEIGHT)
+sf.valprint("PROD_RHO", PROD_RHO)
+
 for j in xrange(TCOL):
+    # MODIFY UPSTREAM BC TO ACCOUNT FOR PRODUCT IN STRING
+    INT[j+1] = DELT * VEL_ARR[-1, j] + INT[j]
+    DEL_HEAD = (DEL_WEIGHT * INT[j+1])/PROD_WEIGHT
+    HEAD_ARR[0, j+1] = HEAD_US + DEL_HEAD  # INCREASE IN HEAD FROM INT MOVEMENT
     for i in xrange(XROW):  # INTERIOR NODE CALCULATIONS
+        fric = sf.FrictionFact_jit(VEL_ARR[i, j], VISC, SPG, DIA_IN, ROUGH, 0)
+        # fric = FRIC
+        rterm = fric*DELX/(2*GRAV*DIA)
+
         cpos = HEAD_ARR[i, j] + BTERM*VEL_ARR[i, j]
         cneg = HEAD_ARR[i+2, j] - BTERM*VEL_ARR[i+2, j]
-        bpos = BTERM + RTERM*np.abs(VEL_ARR[i, j])
-        bneg = BTERM + RTERM*np.abs(VEL_ARR[i+2, j])
+        bpos = BTERM + rterm*np.abs(VEL_ARR[i, j])
+        bneg = BTERM + rterm*np.abs(VEL_ARR[i+2, j])
         HEAD_ARR[i+1, j+1] = (cpos*bneg + cneg*bpos)/(bpos+bneg)
         VEL_ARR[i+1, j+1] = (cpos - cneg)/(bpos + bneg)
+        FRIC_ARR[i+1, j+1] = fric
         if i == 0:  # UPSTREAM BOUNDARY NODE - VELOCITY
             cneg = HEAD_ARR[i+1, j] - BTERM*VEL_ARR[i+1, j]
-            bneg = BTERM + RTERM*np.abs(VEL_ARR[i+1, j])
-            VEL_ARR[i, j+1] = (HEAD_US - cneg)/bneg
+            bneg = BTERM + rterm*np.abs(VEL_ARR[i+1, j])
+            VEL_ARR[i, j+1] = (HEAD_ARR[i, j+1] - cneg)/bneg
         if i == XROW-1:  # DOWNSTREAM BOUNDARY NODE - HEAD
             cpos = HEAD_ARR[i+1, j] + BTERM*VEL_ARR[i+1, j]
-            bpos = BTERM + RTERM*np.abs(VEL_ARR[i+1, j])
+            bpos = BTERM + rterm*np.abs(VEL_ARR[i+1, j])
             HEAD_ARR[-1, j+1] = cpos - bpos*VEL_ARR[-1, j+1]
 
 # CALCULATE PRESSURE
@@ -114,18 +183,33 @@ for i in xrange(COL):
     PRESS[:, i] = (HEAD_ARR[:, i] - HEAD_EL - VEL_ARR[:, i]**2/(2*GRAV))\
                  * GAMMA/144.0
 
+IDX_TIME_4 = PRESS[-1, np.where(TGRID == 4.0)]  # SAVES PRESSURE AT T=4 SEC
+# sf.matprint("PRESSURE AT T=4", IDX_TIME_4)
+
+# SAVES TIME AT MAX P
+IDX_MAX_PRESS = TGRID[np.where(PRESS[-1, :] == max(PRESS[-1, :]))]
+# sf.matprint("TIME AT MAX P", IDX_MAX_PRESS)
+
+# sf.matprint("FRIC ARRAY", FRIC_ARR)
+sf.matprint("INT", INT)
 
 # BUILD ARRAYS FOR PLOTTING
 time_plot = np.tile(TGRID, (NNODE, 1)).T
 x_plot = np.tile(XGRID, (COL, 1)).T
 PATH = '/Users/Lampe/Documents/PB/SurgeAnalysis/Results/'
 
-LBL = [None]*ROW
+START = 0
+STOP = HEAD_ARR.shape[0]
+INC = HEAD_ARR.shape[0]/2
+
+LBL_ALL = [None]*ROW
 for i in xrange(ROW):
-    LBL[i] = "Depth: {:,.0f} ft".format(DEPTH[i])
+    LBL_ALL[i] = "Depth: {:,.0f} ft".format(DEPTH[i])
+
+LBL = LBL_ALL[START:STOP:INC]
 
 FIG1, AX1 = plt.subplots(figsize=(12, 8))
-AX1.plot(time_plot, VEL_ARR.T, 'o--')
+AX1.plot(time_plot[:, START:STOP:INC], VEL_ARR.T[:, START:STOP:INC], 'o--')
 AX1.grid(True)
 AX1.set_xlabel("Time (sec)")
 AX1.set_ylabel("Fluid Velocity (ft/sec)")
@@ -134,10 +218,11 @@ FIG1_NAME = 'fig_velocity.pdf'
 # FIG1.savefig(PATH + FIG1_NAME)
 
 FIG2, AX2 = plt.subplots(figsize=(12, 8))
-AX2.plot(time_plot, PRESS.T, 'o--')
+AX2.plot(time_plot[:, START:STOP:INC], PRESS.T[:, START:STOP:INC], 'o--')
 AX2.grid(True)
 AX2.set_xlabel("Time (sec)")
 AX2.set_ylabel("Pressure (psi)")
 AX2.legend(LBL, frameon=1, framealpha=1, loc=1)
 FIG2_NAME = 'fig_head.pdf'
+plt.show()
 # FIG2.savefig(PATH + FIG2_NAME)
--- SurgeFunc.py
+++ SurgeFunc.py
@@ -1,301 +1,207 @@
-""" 
+"""
     Module to be loaded for the transient surge analysis.
-	Content includes:
-		def valprint(string, value): function for printing numeric values with a label
-		def matprint(string, value): function for printing arrays with a label
-        def valvePercOpen
-		class SS_DeltaH(): general function class for calcuation SS pressure loss
+    Content includes:
+        def valprint(string, value): function for printing numeric values
+        def matprint(string, value): function for printing arrays
+
 """
 
 import numpy as np
-from scipy.interpolate import interp1d
-from numba import jit
+import numba
+import CoolProp.CoolProp as CP
+# from CoolProp.CoolProp import FluidsList
+
 
 def valprint(string, value):
     """ Ensure uniform formatting of scalar value outputs. """
     print("{0:>30}: {1: .6e}".format(string, value))
+
 
 def matprint(string, value):
     """ Ensure uniform formatting of matrix value outputs. """
     print("{0}:".format(string))
     print(value)
 
-@jit(["float32(float32, float32, float32, float32, float32, int32)"], nopython=True)
-def FrictionFact_jit(flowRate, mu, spGrav, ID, roughness, units = 0):
+
+# @numba.jit(["float32(float32, float32, float32, float32, float32, int32)"],
+#            nopython=True)
+@numba.jit(nopython=True)
+def FrictionFact_jit(vel, visc, spgrav, dia_in, roughness, units):
     """
-        All calculations will be performed in consistent SI units.  
-        Therefore, if units are input in US terms (units=0), they will be converted to SI for calculation.
+        All calculations will be performed in consistent SI units.
+        Therefore, if units are input in US terms (units=0),
+        they will be converted to SI for calculation.
 
         Units for Input (must maintain a constant system of units):
 
-                  |   DESCRIPTION      | SI   |
-        units     |system of units     | !=0  |
+                  |   DESCRIPTION      | SI   | US
+        units     |system of units     | !=0  | =0
         ------------------------------------------------
-        flowRate  |volumetric flow rate|m3/sec|
-        dynVisc   |dynamic viscosity   |Pa-s  |
-        spGrav    |specific gravity    |none  |
-        ID        |internal diameter   |m     |
-        roughness |pipe roughness      |m     |
+        VELOCITY  |fliud velocity      |m/sec | ft/sec
+        dynVisc   |dynamic viscosity   |Pa-s  | cP
+        spGrav    |specific gravity    |none  | none
+        ID        |internal diameter   |m     | in
+        roughness |pipe roughness      |m     | in
 
         Output:
         -------------------------------------------------
         f: darcy-weisbach friction factor (dimensionless)
     """
-    rho = spGrav * 1000. # kg/m3
-    area = np.pi * ID **2 / 4.# m2
-    v = flowRate/area # velocity (m/sec)
-    Re = rho * v * ID / mu #reynolds number
-    Re_lim = 2000. #transition point between laminara and turbulent flow
-    f_lim = Re_lim / 64. #maximum friction factor value occurs at Re_lim/64
-    trans = 0.75 #transition range (region of smooth transition)
-    Re_trans = Re_lim*(1+trans) # the transitional zone
-    n = 1.0 #shape factor for lower side of transition (Re < 2000)
-    slope = 10. #shape factor for upper side of transition (Re > 2000)
-    
+
+    if units == 0:  # CONVERT TO SI UNITS
+        visc = visc * 10**-3  # Pa-s
+        vel = vel * 0.3048  # m/sec
+        dia_in = dia_in / 12 * 0.3048  # m
+        roughness = roughness / 12 * 0.3048  # m
+
+    rho = spgrav * 1000.  # kg/m3
+    Re = rho * vel * dia_in / visc  # reynolds number
+    Re_lim = 2000.  # transition point between laminara and turbulent flow
+    f_lim = Re_lim / 64.  # maximum friction factor value occurs at Re_lim/64
+    trans = 0.75  # transition range (region of smooth transition)
+    Re_trans = Re_lim*(1+trans)  # the transitional zone
+    n = 1.0  # shape factor for lower side of transition (Re < 2000)
+    slope = 10.  # shape factor for upper side of transition (Re > 2000)
+
     if Re <= Re_lim:
         f = f_lim * np.sin((np.pi / 2. * (Re / Re_lim))**n)
-        
-    elif Re > Re_lim and Re <= Re_trans: 
+
+    elif Re > Re_lim and Re <= Re_trans:
         # transition between laminar and turbulent flow
-        sqrt_f_trans = 10.0 # initial guess
-        loop_max = 20 # max. number of loops
-        inc = 1 # increment count
-        res = 10.0 # initial value
+        sqrt_f_trans = 10.0  # initial guess
+        loop_max = 20  # max. number of loops
+        inc = 1  # increment count
+        res = 10.0  # initial value
         while res > 10**-8 and inc < loop_max:
-            LHS = -2.0 * np.log10(roughness / (3.7 * ID) + 2.51 / (Re_trans * sqrt_f_trans))
-            res = (LHS - 1./sqrt_f_trans)**2 #squared error
+            LHS = -2.0 * np.log10(roughness / (3.7 * dia_in) +
+                                  2.51 / (Re_trans * sqrt_f_trans))
+            res = (LHS - 1./sqrt_f_trans)**2  # squared error
             sqrt_f_trans = 1./LHS
             inc = inc + 1
         f_trans = sqrt_f_trans**2
         Re_star = (Re - Re_lim) / Re_lim
-        f = f_trans + (f_lim - f_trans)*(1. + slope * Re_star) * np.exp(-slope * Re_star)
-                                       
+        f = f_trans + ((f_lim - f_trans) *
+                       (1. + slope * Re_star) * np.exp(-slope * Re_star))
+
     else:
-        sqrt_f = 10.0 # initial guess
-        loop_max = 20 # max. number of loops
-        inc = 1 # increment count
-        res = 10.0 # initial value
+        sqrt_f = 10.0  # initial guess
+        loop_max = 20  # max. number of loops
+        inc = 1  # increment count
+        res = 10.0  # initial value
 
         while res > 10**-8 and inc < loop_max:
-            LHS = -2.0 * np.log10(roughness / (3.7 * ID) + 2.51 / (Re * sqrt_f))
-            res = (LHS - 1./sqrt_f)**2 #squared error
+            LHS = -2.0 * np.log10(roughness / (3.7 * dia_in) +
+                                  2.51 / (Re * sqrt_f))
+            res = (LHS - 1./sqrt_f)**2  # squared error
             sqrt_f = 1./LHS
             inc = inc + 1
         f = sqrt_f**2
-        
-    return f
 
-@jit
-def MethodChar01_jit(x, t, q0_arr, h0_arr, p0_arr, ca, cf, cb, idx, sg, ID, mu, rough, s, row, col, qp, hp, h, q, p, rho, area, v, Re, f, Tc, tc, y):
-    ID = ID/1000. # convert from mm to m
-    rough = rough/1000. #convert form mm to m
-    area = np.pi * ID **2 / 4.# m2
-    
-    # define initial conditions
-    for i in range(row):
-        q[i,0] = q0_arr[i]
-        h[i,0] = h0_arr[i]
-        p[i,0] = p0_arr[i]
-    
-    # initial values at upstream reservoir
-    # neg characteristic (m3)
-    cn = q[1, 0] - ca[0]*h[1, 0] - cb - cf * q[1, 0] * np.abs(q[1, 0])
-    qp[0] = cn + ca[0]*h[0, 0]
-    
-    # interior nodes
-    for j in range(col - 1):
-        for i in range(row - 2):
-            i = i + 1 # skip first value (BCT)
-            ip1 = i + 1
-            im1 = i - 1
-            ######################## Friction Factor Calc ########################
-            f = FrictionFact_jit(q[ip1,j], mu[idx[i]], sg[idx[i]], ID, rough, units=0)
-            cf = s * f/(2*(ID)*area) #(sec/m3)
-            ########################################################################
-            cn = q[ip1, j] - ca[idx[i]]*h[ip1,j] - cb - cf*q[ip1,j]*np.abs(q[ip1,j]) # negative characteristic
-            cp = q[im1, j] + ca[idx[i]]*h[im1,j] - cb - cf*q[im1,j]*np.abs(q[im1,j]) # positive characteristic
-            qp[i] = 0.5 * (cn + cp) # flow rate at future time
-            hp[i] = (cp - qp[i])/ ca[idx[i]] # head at future time
-            if i == 1: # calculations for upstream reservoir BCTs
-                hp[0] = h[0,0] # Constant upstream reservoir head (m)
-                qp[0] = cn + ca[idx[i]]*hp[0]
-            if i == row - 2: # calculations for downstream valve BCTs
-                #interpolate valve 
-                interp = t[j]
-                if interp >= Tc:
-                    tau = 0
-                else:
-                    I = 0
-                    while interp >= tc[I]:
-                        I = I + 1
-                    frac = (interp - tc[I-1]) / (tc[I] - tc[I-1])
-                    tau = y[I-1] + frac * (y[I] - y[I-1])
-        
-                cv = (tau * q[row-1,0] * q[row-1,0]) / (ca[idx[i]] * h[row-1,0])
-                qp[i+1] = 0.5 * (-cv + ((cv*cv) + 4 * cp * cv)**(0.5))
-                hp[i+1] = (cp - qp[i])/ ca[idx[i]]
-        q[:,j+1] = qp
-        h[:,j+1] = hp
-    return q, h
+    return np.abs(f)
 
-class SS_DeltaH():
-    """ Doc String for General Class should go here"""
-    
-    def __init__(self, flowRate, dynVisc, spGrav, ID, roughness, length, degTheta = 90., units = 0):
-        """
-            General class for calculating the Darcy-Weisbach Friction Factor.
-            
-            All calculations will be performed in consisten SI units.  
-            Therefore, if units are input in US terms (units=0), they will be converted to SI for calculation.
-            
-            Units for Input (must maintain a constant system of units):
-            
-                      |   DESCRIPTION      | US     | SI   |
-            units     |system of units     | =0     | !=0  |
-            ------------------------------------------------
-            flowRate  |volumetric flow rate|gal/min |m3/hr |
-            dynVisc   |dynamic viscosity   |cP      |Pa-s  |
-            spGrav    |specific gravity    |none    |none  |
-            ID        |internal diameter   |inch    |mm    |
-            roughness |pipe roughness      |inch    |mm    |
-            length    |length of flow      |feet    |m     |
-            degTheta  |CCW Angle From Horiz|degree  |degree|
-                      |in + flow direction |
 
-            Note: to obtain results call the method ".Calc()" followed by desired parameter
-            
-            Example: Calculate friciton factor, Reynolds No., head loss, pressure loss, and velocity
+"""
+FluidsList()
+common fluids:
+    Air
+    Argon
+    Benzene
+    n-Butane
+    IsoButane
+    IsoButene
+    Ethane
+    Ethanol
+    Ethylene
+    Methane
+    Nitrogen
+    n-Propane
+    n-Pentane
+    Water
+"""
 
-                f = SS_DeltaH(q, mu, sg, d, rough, L, units=0).Calc().f
-                Re = SS_DeltaH(q, mu, sg, d, rough, L, units=0).Calc().Re
-                dh = SS_DeltaH(q, mu, sg, d, rough, L, units=0).Calc().dh
-                dp = SS_DeltaH(q, mu, sg, d, rough, L, units=0).Calc().dp
-                v = SS_DeltaH(q, mu, sg, d, rough, L, units=0).Calc().v
 
-        """
-        self.Q = flowRate
-        self.mu = dynVisc
-        self.sg = spGrav
-        self.ID = ID
-        self.roughness = roughness
-        self.L = length
-        self.units = units
-        self.theta = np.pi / 180. * degTheta
-        
-        
-    def ConvertIn(self):
-        """
-        Calculations will all be performed in SI units; therefore, this method converts from US to SI units
-        """
-        self.rho = self.sg * 1000 #kg/m3
-        if self.units == 0:
-            #convert from US to SI units
-            self.L = self.L * 0.3048 #m
-            self.Q = self.Q *3.7853/1000. / 60. # m3/sec
-            self.ID = self.ID * 2.54 / 100. # m
-            self.area = np.pi * self.ID **2 / 4.# m2
-            self.v = self.Q / self.area # m/sec
-            self.mu = self.mu * 10**-3 # Pa-s
-            self.roughness = self.roughness * 2.54 / 100. # m
-            textOut = "Input Was US Units"
-        else:
-            #convert to consistent SI units
-            self.Q = self.Q / 3600. # m3/sec
-            self.ID = self.ID / 1000. # m
-            self.area = np.pi * self.ID **2 / 4.# m2
-            self.v = self.Q / self.area # m/sec
-            self.roughness = self.roughness / 1000. #m
-            textOut = "Input Was SI Units"
-        return self
-    
-    def Re(self):
-        """
-        Calculates Reynolds Number
-         - this method may be called to check the Reynolds Number
-        """
-        self.Re = self.rho * self.v * self.ID / self.mu
-        return self
-        
-    def FricFact(self):
-        """
-        Function to obtain the Darcy-Weisbach friction factor:
-            Laminar Flow (Re <= 2000) -> Poiseuille's relationship
-            Turbulent Flow (Re > 2000) -> evaluate the colebrrok-white equation 
+def visc(temp_f, press_psig, fluid):
+    """
+    Function used to call CoolProp to obtain fluid viscosity (cP).
+    Input Arguments:
+        temp_f = temperature of fluid (F)
+        press_psig = pressure of fluid (psig)
+        fluid = fluid from CoolProp list of fluids,
+            argument must be a string
 
-        Input:
-        -------------------------------------------------
-        Re (required): reynolds number (dimensionless)
-        d  (required): hydraulic diameter (length)
-        epsilon (required): conduit roughness (length)
+    list of predefined fluids may be found at:
+    http://www.coolprop.org/fluid_properties/PurePseudoPure.html
 
-        Output:
-        -------------------------------------------------
-        f: darcy-weisbach friction factor (dimensionless)
-        """
-        if self.Re <= 2000.:
-            f = self.Re / 64.
-        else:
-            sqrt_f = 10.0 # initial guess
-            loop_max = 20 # max. number of loops
-            inc = 1 # increment count
-            res = 10.0 # initial value
+    list of properties may be found at:
+    http://www.coolprop.org/v4/apidoc/CoolProp.html
+    """
 
-            while res > 10**-8 and inc < loop_max:
-                LHS = -2.0 * np.log10(self.roughness / (3.7 * self.ID) + 2.51 / (self.Re * sqrt_f))
-                res = (LHS - 1./sqrt_f)**2 #squared error
-                sqrt_f = 1./LHS
-                inc = inc + 1
-            f = sqrt_f**2
+    temp = (temp_f - 32)*5/9 + 273.15  # kelvin
+    p_atm = 101325.0  # pascal (atmospheric pressure at sea level)
+    press = press_psig * (101325/14.6959) + p_atm  # pascal abs
+    fluid = str(fluid)
+    visc_pas = CP.PropsSI('viscosity', 'T', temp, 'P', press, fluid)  # Pa-s
+    visc_cp = visc_pas * 10**3  # centipoise
+    return visc_cp
 
-            if inc == loop_max:
-                outText = "Friction Factor Did NOT Converge! Printed Value Should = 1.0"
-                print(outText)
-                print(-2.0 * np.log10(self.roughness / (3.7 * self.ID) + 2.51 / (self.Re * f**0.5)) * f**0.5)
-                
-        self.f = f
-        return self
-    
-    def HeadLoss(self):
-        """
-        Calculates the change in piezometric head for steady-state flow
-        accounts for head loss resulting from:
-            -friction
-            -change in elevation
-        """
-        g = 9.806 #m/s2
-        self.dh_m = -self.f * (self.Q)**2 * self.L / (2 * self.ID * self.area**2*g) - np.sin(self.theta)*self.L
-        self.dp_Pa = self.rho * g * self.dh_m
-        return self
-    
-    def ConvertOut(self):
-        """
-        Converts units for return to user
-        US Units:
-            - head loss: feet
-            - pressure loss: psi
-            - velocity: ft/sec
-        SI Units:
-            - head loss: meter
-            - pressure loss: MPa
-            - velocity: m/sec
-        """
-        if self.units == 0:
-            self.dh = self.dh_m / 0.3048 # feet
-            self.dp = self.dp_Pa * 1.4504 * 10**-4 # psi
-            self.v = self.v /0.3048 # ft/sec
-        else:
-            self.dh = self.dh_m # m
-            self.dp = self.dp_Pa / 10**6 # MPa
-            self.v = self.v # m/sec
-        return self
-                       
-    def Calc(self):
-        """
-        This method must be called to obtain any needed parameters
-        """
-        self.ConvertIn()
-        self.Re()
-        self.FricFact()
-        self.HeadLoss()
-        self.ConvertOut()
-        return self
+
+def unit_weight(temp_f, press_psig, fluid):
+    """
+    temp_f = temperature of fluid (F)
+    press_psig = pressure of fluid (psig)
+    fluid = fluid from CoolProp list of fluids,
+        argument must be a string
+
+    list of predefined fluids may be found at:
+    http://www.coolprop.org/fluid_properties/PurePseudoPure.html
+    """
+
+    temp = (temp_f - 32)*5/9 + 273.15  # kelvin
+    p_atm = 101325.0  # pascal (atmospheric pressure at sea level)
+    press = press_psig * (101325/14.6959) + p_atm  # pascal abs
+    fluid = str(fluid)
+    den_kgm3 = CP.PropsSI('D', 'T', temp, 'P', press, fluid)  # kg/m3
+    unit_lbft3 = den_kgm3/1000 * 62.4  # lb/ft3
+    return unit_lbft3
+
+
+def speedOfSound(temp_f, press_psig, fluid):
+    """
+    temp_f = temperature of fluid (F)
+    press_psig = pressure of fluid (psig)
+    fluid = fluid from CoolProp list of fluids,
+        argument must be a string
+
+    list of predefined fluids may be found at:
+    http://www.coolprop.org/fluid_properties/PurePseudoPure.html
+    """
+    temp = (temp_f - 32)*5/9 + 273.15  # kelvin
+    p_atm = 101325.0  # pascal (atmospheric pressure at sea level)
+    press = press_psig * (101325/14.6959) + p_atm  # pascal abs
+    fluid = str(fluid)
+    a_mps = CP.PropsSI('A', 'T', temp, 'P', press, fluid)  # m/sec
+    a_fps = a_mps / 0.3048  # feet per sec
+    return a_fps
+
+
+def compressibility(temp_f, press_psig, fluid):
+    """
+    temp_f = temperature of fluid (F)
+    press_psig = pressure of fluid (psig)
+    fluid = fluid from CoolProp list of fluids,
+        argument must be a string
+
+    list of predefined fluids may be found at:
+    http://www.coolprop.org/fluid_properties/PurePseudoPure.html
+    """
+
+    temp = (temp_f - 32)*5/9 + 273.15  # kelvin
+    p_atm = 101325.0  # Pa (atmospheric pressure at sea level)
+    press = press_psig * (101325/14.6959) + p_atm  # pascal abs
+    fluid = str(fluid)
+    comp_kpa = CP.PropsSI('isothermal_compressibility', 'T', temp,
+                          'P', press, fluid)  # 1/Pa
+    comp_psi = comp_kpa * (101325.0/14.6959)  # 1/psi
+    return comp_psi
+
+
